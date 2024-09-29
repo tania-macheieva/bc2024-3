62633b9d72c411c7d81afa77ff2077492fe5734d
tania-macheieva bc2024-3 <macheieva.tetiana@gmail.com>
2024-09-29 15:24:58 +0300

    Done the first part of the lab

---
+++ index.js
@@ -0,0 +1,46 @@
+let fs = require('node:fs');
+const { command } = require('commander');
+
+
+const program = new command();
+
+program
+    .requireOption('-i, --input - (обовʼязковий параметр) шлях до файлу, який даємо для читання (json з даними серверу Національного банку України)')
+    .option('-o, --output - (не обовʼязковий параметр) шлях до файлу, у якому записуємо результат')
+    .option('-d, --display - (не обовʼязковий параметр) якщо задано цей параметр, то результат має бути виведено у консоль')
+    .parse(process.argv);
+
+const options = program.opts();
+
+
+//Check if input file exists
+if(!options.input) {
+    console.error("Please, specify input file");
+    program.exit(1);
+}
+
+//Read data from input file
+const data = fs.readFileSync(options.input, 'utf-8');
+const jsonData = JSON.parse(data);
+
+// Check if input file exists
+if (!fs.existsSync(options.input)) {
+    console.error("Cannot find input file");
+    process.exit(1);
+  }  
+
+// Display data in console if -d is specified
+if (options.display) {
+    console.log(jsonData);
+}
+
+// Write data to output file if -o is specified
+if (options.output) {
+    fs.writeFileSync(options.output, JSON.stringify(jsonData, null, 2));
+    
+    // Display data in console again if -d is specified
+    if (options.display) {
+      console.log(jsonData);
+    }
+}
+  
